0628af9b93a54e8e23202f3fdd196ec557176115
SUMYAK JAIN <3456gaurav@gmail.com>
2020-04-29 03:21:59 +0530

    Update admin.java

--- ADMIN/admin.java
+++ ADMIN/admin.java
@@ -5,6 +5,8 @@
  */
 package rapid.rentals;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author GAURAV
@@ -34,43 +36,39 @@ public class admin extends javax.swing.JFrame {
         enter_passwd = new javax.swing.JTextField();
         login_btn = new javax.swing.JToggleButton();
         logout_btn = new javax.swing.JToggleButton();
+        validation = new javax.swing.JLabel();
 
         jToggleButton2.setText("jToggleButton2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Rapid Rentals");
-        getContentPane().setLayout(null);
+        setPreferredSize(new java.awt.Dimension(440, 370));
 
         name_txt.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         name_txt.setText("Name");
-        getContentPane().add(name_txt);
-        name_txt.setBounds(122, 85, 40, 17);
 
         name_enter.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 name_enterActionPerformed(evt);
             }
         });
-        getContentPane().add(name_enter);
-        name_enter.setBounds(180, 85, 98, 20);
 
         passwd_txt.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         passwd_txt.setText("Password");
-        getContentPane().add(passwd_txt);
-        passwd_txt.setBounds(95, 123, 67, 17);
 
         enter_passwd.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 enter_passwdActionPerformed(evt);
             }
         });
-        getContentPane().add(enter_passwd);
-        enter_passwd.setBounds(180, 123, 98, 20);
 
         login_btn.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         login_btn.setText("LOGIN");
-        getContentPane().add(login_btn);
-        login_btn.setBounds(132, 174, 91, 25);
+        login_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                login_btnActionPerformed(evt);
+            }
+        });
 
         logout_btn.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         logout_btn.setText("Go Back");
@@ -79,8 +77,55 @@ public class admin extends javax.swing.JFrame {
                 logout_btnActionPerformed(evt);
             }
         });
-        getContentPane().add(logout_btn);
-        logout_btn.setBounds(19, 264, 89, 25);
+
+        validation.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        validation.setForeground(new java.awt.Color(255, 0, 0));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(122, 122, 122)
+                        .addComponent(name_txt)
+                        .addGap(18, 18, 18)
+                        .addComponent(name_enter, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(132, 132, 132)
+                        .addComponent(login_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addComponent(logout_btn))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(95, 95, 95)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(validation, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(passwd_txt)
+                                .addGap(18, 18, 18)
+                                .addComponent(enter_passwd, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGap(122, 122, 122))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(85, 85, 85)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(name_txt)
+                    .addComponent(name_enter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(passwd_txt)
+                    .addComponent(enter_passwd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addComponent(login_btn)
+                .addGap(11, 11, 11)
+                .addComponent(validation, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addComponent(logout_btn))
+        );
 
         pack();
     }// </editor-fold>                        
@@ -96,6 +141,16 @@ public class admin extends javax.swing.JFrame {
     private void logout_btnActionPerformed(java.awt.event.ActionEvent evt) {                                           
         // TODO add your handling code here:
     }                                          
+
+    private void login_btnActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        if(name_enter.getText().equals("ADMIN123") && enter_passwd.getText().equals("01234") ){
+        admin_options obj=new admin_options();
+        obj.setVisible(true);
+        }
+        else{
+            JOptionPane.showMessageDialog(this,"Invalid Login Details");
+        }
+    }                                         
 
     /**
      * @param args the command line arguments
@@ -140,5 +195,6 @@ public class admin extends javax.swing.JFrame {
     private javax.swing.JTextField name_enter;
     private javax.swing.JLabel name_txt;
     private javax.swing.JLabel passwd_txt;
+    private javax.swing.JLabel validation;
     // End of variables declaration                   
 }
