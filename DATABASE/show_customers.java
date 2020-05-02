ce9aeaa2559c74e021986ce9ad2616edce048181
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-02 17:04:19 +0530

    Create show_customers.java

---
+++ DATABASE/show_customers.java
@@ -0,0 +1,32 @@
+package rapid.rentals;
+import net.proteanit.sql.DbUtils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class show_customers extends javax.swing.JFrame {
+
+    /**
+     * Creates new form show_customers
+     */
+    public show_customers() {
+        initComponents();
+        DisplayTable();
+    }
+
+    private void DisplayTable(){
+        try{
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/login","root","01234");
+           String loginTable="SELECT * FROM login.login_info" ;
+           PreparedStatement psmt=conn.prepareStatement(loginTable);
+           ResultSet rs=psmt.executeQuery();
+           login_table.setModel(DbUtils.resultSetToTableModel(rs));
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
